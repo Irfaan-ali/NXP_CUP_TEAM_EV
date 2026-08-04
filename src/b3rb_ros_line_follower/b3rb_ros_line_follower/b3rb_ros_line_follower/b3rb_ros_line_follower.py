e443bbc89e7247979ddb7192bc8d24cb0c6c92c5
Irfaan <irfaanali2806@gmail.com>
2026-08-04 21:18:22 +0530

    replaced terminal flooding messages

--- src/b3rb_ros_line_follower/b3rb_ros_line_follower/b3rb_ros_line_follower/b3rb_ros_line_follower.py
+++ src/b3rb_ros_line_follower/b3rb_ros_line_follower/b3rb_ros_line_follower/b3rb_ros_line_follower.py
@@ -179,7 +179,7 @@ class LineFollower(Node):
 
     def edge_vectors_callback(self, message):
 
-        self.get_logger().info(f"Vectors detected : {message.vector_count}")
+        self.get_logger().debug(f"Vectors detected : {message.vector_count}")
 
         if self.obstacle_in_front:
             return
@@ -288,7 +288,7 @@ class LineFollower(Node):
         self.lane_speed = speed
         self.lane_turn = turn
 
-        self.get_logger().info(f"Error={error:.1f}  Turn={turn:.2f}  Speed={speed:.2f}")
+        self.get_logger().debug(f"Error={error:.1f}  Turn={turn:.2f}  Speed={speed:.2f}")
 
 
     def lidar_callback(self, message):
@@ -402,7 +402,7 @@ class LineFollower(Node):
         # Debug Information
         # -------------------------------------------------------
 
-        self.get_logger().info(
+        self.get_logger().debug(
             f"Front={front_dist:.2f}m | "
             f"Left={left_dist:.2f}m | "
             f"Right={right_dist:.2f}m | "
