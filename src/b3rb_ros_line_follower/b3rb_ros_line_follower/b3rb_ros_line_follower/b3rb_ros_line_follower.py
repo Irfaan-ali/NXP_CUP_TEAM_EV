773634a04f181adc7dadbb0bf265e10c6a68dc11
Irfaan <irfaanali2806@gmail.com>
2026-08-04 23:10:25 +0530

    modified one vector

--- src/b3rb_ros_line_follower/b3rb_ros_line_follower/b3rb_ros_line_follower/b3rb_ros_line_follower.py
+++ src/b3rb_ros_line_follower/b3rb_ros_line_follower/b3rb_ros_line_follower/b3rb_ros_line_follower.py
@@ -224,14 +224,7 @@ class LineFollower(Node):
         # ----------------------------------------------------
         else:
 
-            v1_length = math.hypot(message.vector_1[1].x - message.vector_1[0].x,message.vector_1[1].y - message.vector_1[0].y)
-
-            v2_length = math.hypot(message.vector_2[1].x - message.vector_2[0].x,message.vector_2[1].y - message.vector_2[0].y)
-
-            if v1_length > v2_length:
-                lane = message.vector_1
-            else:
-                lane = message.vector_2
+            lane = message.vector_1
 
             if lane[1].x < image_center:
 
